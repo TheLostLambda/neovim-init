22f161f7a58921f450a51f9545004fd54deb74b1
Brooks J Rady <b.j.rady@gmail.com>
2022-03-28 00:17:28 +0100

    A couple of tweaks

--- init.lua
+++ init.lua
@@ -6,75 +6,83 @@ local opt = vim.opt  -- To set options
 
 -- Install Packages
 require 'paq' {
-    'savq/paq-nvim';                      -- Manage the package manager
-    'sainnhe/gruvbox-material';           -- A nice colorscheme
-    'hoob3rt/lualine.nvim';               -- A snazzy status-line
-    'nvim-treesitter/nvim-treesitter';    -- Support for better syntax highlighting
-    'neovim/nvim-lspconfig';              -- Automatically launch LSP servers
-    'nvim-lua/lsp_extensions.nvim';       -- Enable LSP protocol extensions
-    'hrsh7th/nvim-compe';                 -- Enable completions for LSP (Update to nvim-cmp!)
-    'ray-x/lsp_signature.nvim';           -- Enable function parameter hints in LSP
-    'tami5/lspsaga.nvim';                 -- Add some nice UI components to LSP
-    'RRethy/vim-illuminate';              -- Use LSP to highlight hovered symbols
-    'blackCauldron7/surround.nvim';       -- Pair brackets and surround selections
-    'hrsh7th/vim-vsnip';                  -- Basic snippet support
-    'rafamadriz/friendly-snippets';       -- A collection of community snippets
-    'nvim-telescope/telescope.nvim';      -- A powerful fuzzy-finder
-    'nvim-lua/plenary.nvim';              -- A dependency for Telescope
-    'nvim-lua/popup.nvim';                -- Another dependency for Telescope
-    'folke/which-key.nvim';               -- An way to show all available keybinds
-    'folke/trouble.nvim';                 -- Summarizes errors and lints
-    'kyazdani42/nvim-web-devicons';       -- Add some nice icons for filetypes
+  'savq/paq-nvim';                    -- Manage the package manager
+  'sainnhe/gruvbox-material';         -- A nice colorscheme
+  'hoob3rt/lualine.nvim';             -- A snazzy status-line
+  'nvim-treesitter/nvim-treesitter';  -- Support for better syntax highlighting
+  'neovim/nvim-lspconfig';            -- Automatically launch LSP servers
+  'nvim-lua/lsp_extensions.nvim';     -- Enable LSP protocol extensions
+  'hrsh7th/nvim-compe';               -- Enable completions for LSP (Update to nvim-cmp!)
+  'ray-x/lsp_signature.nvim';         -- Enable function parameter hints in LSP
+  'tami5/lspsaga.nvim';               -- Add some nice UI components to LSP
+  'RRethy/vim-illuminate';            -- Use LSP to highlight hovered symbols
+  'hrsh7th/vim-vsnip';                -- Basic snippet support
+  'rafamadriz/friendly-snippets';     -- A collection of community snippets
+  'nvim-telescope/telescope.nvim';    -- A powerful fuzzy-finder
+  'nvim-lua/plenary.nvim';            -- A dependency for Telescope
+  'nvim-lua/popup.nvim';              -- Another dependency for Telescope
+  'folke/which-key.nvim';             -- An way to show all available keybinds
+  'folke/trouble.nvim';               -- Summarizes errors and lints
+  'kyazdani42/nvim-web-devicons';     -- Add some nice icons for filetypes
+  'windwp/nvim-autopairs';            -- Automatically pair some characters
+  'Olical/conjure';                   -- Add lovely lisp support
+  'clojure-vim/vim-jack-in';          -- Add CIDER middleware for Clojure
+  'tpope/vim-dispatch';               -- A dependency for `vim-jack-in`
+  'radenling/vim-dispatch-neovim';    -- A neovim patch for `vim-dispatch`
 }
 
 -- Built-In Options
-opt.expandtab = true          -- Use spaces instead of tabs
-opt.hidden = true             -- Enable background buffers
-opt.ignorecase = true         -- Ignore case
-opt.joinspaces = false        -- No double spaces with join
-opt.list = true               -- Show some invisible characters
-opt.number = true             -- Show line numbers
-opt.relativenumber = true     -- Relative line numbers
-opt.scrolloff = 4             -- Lines of context
-opt.shiftround = true         -- Round indent
-opt.shiftwidth = 2            -- Size of an indent
-opt.sidescrolloff = 8         -- Columns of context
-opt.smartcase = true          -- Do not ignore case with capitals
-opt.smartindent = true        -- Insert indents automatically
-opt.splitbelow = true         -- Put new windows below current
-opt.splitright = true         -- Put new windows right of current
-opt.tabstop = 2               -- Number of spaces tabs count for
-opt.termguicolors = true      -- True color support
-opt.wrap = false              -- Disable line wrap
-opt.cursorline = true         -- Highlight the selected line
-opt.signcolumn = 'yes'        -- Show diagnostics in their own column
-opt.spell = true              -- Enable spell checking
-opt.spelllang = 'en_gb'       -- Set language to English
-opt.updatetime = 750          -- Trigger timeouts every 750ms
-opt.autoread = true           -- Automatically reload changed files
-opt.swapfile = false          -- Disable the swap files (allow multiple access)
-opt.clipboard = 'unnamedplus' -- Use the system clipboard
+opt.expandtab = true           -- Use spaces instead of tabs
+opt.hidden = true              -- Enable background buffers
+opt.ignorecase = true          -- Ignore case
+opt.joinspaces = false         -- No double spaces with join
+opt.list = true                -- Show some invisible characters
+opt.number = true              -- Show line numbers
+opt.relativenumber = true      -- Relative line numbers
+opt.scrolloff = 4              -- Lines of context
+opt.shiftround = true          -- Round indent
+opt.shiftwidth = 2             -- Size of an indent
+opt.sidescrolloff = 8          -- Columns of context
+opt.smartcase = true           -- Do not ignore case with capitals
+opt.smartindent = true         -- Insert indents automatically
+opt.splitbelow = true          -- Put new windows below current
+opt.splitright = true          -- Put new windows right of current
+opt.tabstop = 2                -- Number of spaces tabs count for
+opt.termguicolors = true       -- True color support
+opt.wrap = false               -- Disable line wrap
+opt.cursorline = true          -- Highlight the selected line
+opt.signcolumn = 'yes'         -- Show diagnostics in their own column
+opt.spell = true               -- Enable spell checking
+opt.spelllang = 'en_gb'        -- Set language to English
+opt.updatetime = 750           -- Trigger timeouts every 750ms
+opt.autoread = true            -- Automatically reload changed files
+opt.swapfile = false           -- Disable the swap files (allow multiple access)
+opt.clipboard = 'unnamedplus'  -- Use the system clipboard
 
 -- Make Things Look Nice
 cmd 'colorscheme gruvbox-material'
 
 -- Set Up Intelligent Highlighting & Indentation
 require('nvim-treesitter.configs').setup {
-    ensure_installed = 'maintained',
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true
-    },
-    indent = {enable = true},
+  ensure_installed = 'maintained',
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true
+  },
+  indent = {enable = true},
 }
+
+-- Treesitter Based Folding
+opt.foldmethod = 'expr'
+opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
 -- Set Up LSP UI & Servers
 require('lspsaga').init_lsp_saga()
 
 local universal_config = {
-    on_attach = function(client) 
-        require('illuminate').on_attach(client)
-    end
+  on_attach = function(client)
+    require('illuminate').on_attach(client)
+  end
 }
 
 local lsp = require('lspconfig')
@@ -97,30 +105,35 @@ require('lualine').setup()
 opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 opt.shortmess:append({ c = true })
 require('lsp_signature').on_attach {
-    hint_prefix = '',
-    use_lspsaga = true
+  hint_prefix = '',
+  use_lspsaga = true
 }
 
 require('compe').setup {
-    source = {
-        path = true;
-        buffer = true;
-        calc = true;
-        nvim_lsp = true;
-        nvim_lua = true;
-        vsnip = true;
-    },
-    preselect = 'always'
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+  },
+  preselect = 'always'
 }
 
 -- Complete Bracket Pairs
-require('surround').setup{}
+require('nvim-autopairs').setup()
+require("nvim-autopairs.completion.compe").setup({
+  map_cr = true,        -- Map <CR> on insert mode
+  map_complete = true,  -- Auto insert `(` after a function or method item
+  auto_select = true,   -- Auto select first item
+})
 
 -- Set Up Which-Key To Show Keybindings & Spelling
 require('which-key').setup {
-    plugins = {
-        spelling = {enabled = true}
-    }
+  plugins = {
+    spelling = {enabled = true}
+  }
 }
 
 -- Summarise Buffer Diagnostics
@@ -128,9 +141,9 @@ require('trouble').setup()
 
 -- Enable Some LSP Protocol Extensions For Rust
 inlay_hints = {
-    prefix = '',
-    highlight = 'NonText',
-    enabled = {'TypeHint', 'ChainingHint'}
+  prefix = '',
+  highlight = 'NonText',
+  enabled = {'TypeHint', 'ChainingHint'}
 }
 
 -- Keep an eye on this https://github.com/neovim/neovim/pull/14661
@@ -156,7 +169,7 @@ local n = {noremap = true}
 local e = {expr = true}
 
 -- Tab Completion
-map('i', '<CR>',    'pumvisible() ? compe#close() . "\\<CR>" : "\\<CR>"', e)
+--map('i', '<CR>',    'pumvisible() ? compe#close() . "\\<CR>" : "\\<CR>"', e)
 map('i', '<Tab>',   'compe#confirm("<Tab>")', e)
 map('i', '<S-Tab>', '<C-n>', e)
 
